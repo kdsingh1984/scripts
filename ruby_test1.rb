e63958a55cbb55625b50ff9f60494ca789abcc55
kdsingh1984 <kdsingh1984@gmail.com>
2014-11-23 08:47:53 +0200

    Added global and instance variable

--- ruby_test1.rb
+++ ruby_test1.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/ruby -w
-#
+#Just for fun testing everything mentioned in
+#http://www.tutorialspoint.com/ruby/ruby_syntax.htm
+
 END {
     puts "No more work"
     }
@@ -7,3 +9,41 @@ puts "Hello All"
 BEGIN {
     puts "This is first test script"
     }
+
+class Sample
+    #This is Sample Class
+    def hello(id, name, address)
+        puts "Hello #{name} your ID is #{id}"
+    end
+end
+
+obj1 = Sample. new
+obj1.hello(1,'kamal','New Delhi')
+
+$totalnumber=10
+class Testglobalvar
+    # this class is for testing global variables
+    def print_number
+        puts "There are #$totalnumber apples in bucket"
+    end
+end
+
+Testobj1 = Testglobalvar.new
+Testobj1.print_number
+
+#Below example is for Instance Variable
+#Instance variable starts with @
+#
+class Instancevar
+    def initialize(name, number, email)
+        @cust_name=name
+        @cust_number=number
+        @cust_email=email
+    end
+    def print_details()
+        puts "Customer name is #@cust_name"
+        puts "Customer numebr is #@cust_number"
+    end
+end
+custobj1=Instancevar.new('Kamal','1234','hdjf@djbf')
+custobj1.print_details()
