a2fb537268daacda49bbbe3d3746dd4e7b78a639
kdsingh1984 <kdsingh1984@gmail.com>
2014-11-30 03:39:41 +0200

    Till module and methods

--- ruby_test1.rb
+++ ruby_test1.rb
@@ -122,3 +122,100 @@ end
 # double colons '::' is used for calling constant from class 
 # 
 puts Testconstant::VAR1
+
+#if loop example:
+x=1
+if x > 2
+   puts "x is greater than 2"
+elsif x <= 2 and x!=0
+   puts "x is 1"
+else
+   puts "I can't guess the number"
+end
+
+$debug=1
+print "debug\n" if $debug
+
+unless x > 2
+    print "x is less then 2\n"
+else
+    print "x is grt then 2\n"
+end
+$kamal=nil
+print "value is less\n" unless $kamal
+
+#Ruby Case statement
+#case experssion is compared with when expression and code separated from when expression using then,newline or semi-colon
+$age=6
+case $age
+when 0..2 then print "This is Baby age\n"
+when 2..5 then print "This is Little Child\n"
+else print "These are kids\n"
+end
+
+    #While statement
+    # While separated from code using do, newline or Semicolon
+$i=0
+$num=5
+    while $i < $num
+        puts "Number in loop is #$i"
+        $i += 1
+    end
+    
+$start=0
+$end=5
+until $start > $end
+    puts "Start is still #$start"
+    $start += 1
+end
+#break: it will terminate a method with an associated block.
+#next: jump to next iteration of most recent loop.
+for i in 0..5
+    if i > 2
+        break
+    end
+    puts "Value of i is #{i}, which is less then 3"
+end
+for i in 0..5
+    if i <= 2
+        next
+    end
+    puts "Value of i is #{i}, which is greater then 2"
+end
+
+#Ruby Method
+#Reuby methods are just like functions in any other language
+def first_method(a1,a2)
+    puts "Value of arguments are #{a1} and #{a2}"
+end
+first_method 1,2
+
+#Ruby allows to declare * number of arguments, Here is Example
+def variable_test(*test)
+    puts "Total defined variables are #{test.length}"
+    for i in 0...test.length
+        puts "Value of variable is #{test[i]}"
+    end
+end
+
+variable_test 'kamal','jass','seerat'
+
+#Ruby class, object, alias, undef
+#alias: it give alias to method or global variable. it keeps current defination even if method is modified.
+#undef: it cancel the method defination.
+#
+#Ruby Modules
+#Modules is a way to group together classes,methods,constants without any issue of name clashes
+#Modules name start with upper case letter. define method start with module name with . and methoud name
+module Testmodule
+    VAR1=1
+    def Testmodule.method1(a1,a2)
+        puts "I am method from module Testmodule with arguments #{a1} and #{a2}"
+    end
+end
+
+Testmodule.method1('kamal','jass')
+puts "constant in Testmodule is #{Testmodule::VAR1}"
+
+#require: it is same like include in python
+#
